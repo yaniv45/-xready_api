cb7089f497366adf4aeec13ad973a0ef66c90ae7
yaniv45 <yanivasraf45@gmail.com>
2025-07-10 11:03:58 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>ברוך הבא ל־X-Ready</h1>
-      <p>האפליקציה מוכנה, התכנים נשמרים ומנותחים!</p>
-    </div>
-  );
-}
