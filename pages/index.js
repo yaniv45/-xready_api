a0084e42693d3863fef675748ec36fa65eb4d57c
yaniv45 <yanivasraf45@gmail.com>
2025-07-10 10:48:55 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>ברוך הבא ל־X-Ready</h1>
-      <p>האפליקציה מוכנה, הנתונים נשמרים ומנותחים!</p>
-    </div>
-  );
-}
+
